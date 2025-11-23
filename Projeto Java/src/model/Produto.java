dcc3c8470ff6c1fde75dd1673f68198b625a4c44
victhoriaellen <vucthoriadenyver@gmail.com>
2025-11-23 14:21:03 -0300

    Add files via upload

---
+++ Projeto Java/src/model/Produto.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    public Produto(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public String getNome() { return nome; }
+    public double getPreco() { return preco; }
+    public int getEstoque() { return estoque; }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " | R$" + preco + " | Estoque: " + estoque;
+    }
+}
