c058fe7f17a3b892e7df998e3bd2966c676ae6c4
victhoriaellen <vucthoriadenyver@gmail.com>
2025-11-30 16:43:23 -0300

    Add files via upload

---
+++ Projeto Java/src/service/Sessao (1).java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Cliente;
+import model.Mercado;
+
+public class Sessao {
+
+    public static Mercado mercadoLogado = null;
+    public static Cliente clienteLogado = null;
+    public static CompraService compraService = new CompraService();
+
+
+    public static void sair() {
+        mercadoLogado = null;
+        clienteLogado = null;
+    }
+}
