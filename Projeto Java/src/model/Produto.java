cd5cfbb153d8f2d6149bb07471772a2295fa822e
victhoriaellen <vucthoriadenyver@gmail.com>
2025-11-30 14:24:15 -0300

    Refactor Produto class to implement IProduto interface

--- Projeto Java/src/model/Produto.java
+++ Projeto Java/src/model/Produto.java
@@ -1,27 +1,51 @@
 package model;
 
-public class Produto {
+import enums.CategoriaProduto;
+import model.interfaces.IProduto;
+
+public class Produto implements IProduto {
 
     private String nome;
     private double preco;
     private int estoque;
+    private CategoriaProduto categoria;
+    private Mercado mercado;
 
-    public Produto(String nome, double preco, int estoque) {
+    public Produto(String nome, double preco, int estoque, CategoriaProduto categoria) {
         this.nome = nome;
         this.preco = preco;
         this.estoque = estoque;
-    }
-
-    public String getNome() { return nome; }
-    public double getPreco() { return preco; }
-    public int getEstoque() { return estoque; }
-
-    public void setEstoque(int estoque) {
-        this.estoque = estoque;
+        this.categoria = categoria;
     }
 
     @Override
+    public Mercado getMercado() {
+        return mercado;
+    }
+
+    public void setMercado(Mercado mercado) {
+        this.mercado = mercado;
+    }
+
+    @Override
+    public String getNome() { return nome; }
+
+    @Override
+    public double getPreco() { return preco; }
+
+    @Override
+    public int getEstoque() { return estoque; }
+
+    @Override
+    public CategoriaProduto getCategoria() { return categoria; }
+
+    public void reduzirEstoque(int qtd) { estoque -= qtd; }
+
+    @Override
     public String toString() {
-        return nome + " | R$" + preco + " | Estoque: " + estoque;
+        return "Produto: " + nome +
+                " | Preço: " + preco +
+                " | Estoque: " + estoque +
+                " | Categoria: " + categoria;
     }
 }
